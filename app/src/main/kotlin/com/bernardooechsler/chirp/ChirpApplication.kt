cbb804fe10b63e06505a415e93bf233004836c9b
bernardo <bernardo.oechsler6@gmail,com>
2026-03-04 19:18:15 +0000

    websockt part 1 impl

--- app/src/main/kotlin/com/bernardooechsler/chirp/ChirpApplication.kt
+++ app/src/main/kotlin/com/bernardooechsler/chirp/ChirpApplication.kt
@@ -1,32 +1,13 @@
 package com.bernardooechsler.chirp
 
-import com.bernardooechsler.chirp.infra.database.entities.UserEntity
-import com.bernardooechsler.chirp.infra.database.repositories.UserRepository
-import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Component
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class ChirpApplication
 
 fun main(args: Array<String>) {
 	runApplication<ChirpApplication>(*args)
 }
-
-//@Component
-//class Demo(
-//    private val repository: UserRepository
-//) {
-//
-//    @PostConstruct
-//    fun init() {
-//        repository.save(
-//            UserEntity(
-//                email = "bernardo@gmail.com",
-//                username = "pika",
-//                hashedPassword = "12312312321"
-//            )
-//        )
-//    }
-//}
